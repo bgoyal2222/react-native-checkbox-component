ed771702b747c32eb3a6375913aff6a6d6597682
Bhavesh Goyal <bhavesh.goyal@zaggle.in>
2018-06-26 17:32:35 +0530

    v1.0.1

---
+++ README.md
@@ -0,0 +1,83 @@
+# react-native-checkbox-component
+[ ![release](https://img.shields.io/badge/release-v1.0.1-blue.svgmaxAge=2592000?style=flat-square)](https://github.com/bgoyal2222/react-native-checkbox-component/releases)
+[ ![PRs Welcome](https://img.shields.io/badge/PRs-Welcome-brightgreen.svg)](https://github.com/bgoyal2222/react-native-checkbox-component/pulls)
+[![License MIT](http://img.shields.io/badge/license-MIT-orange.svg?style=flat)](https://github.com/bgoyal2222/react-native-checkbox-component/blob/master/LICENSE)
+
+
+
+Checkbox component for react native, it works on iOS and Android.
+
+## Content
+
+- [Installation](#installation)
+- [Getting started](#getting-started)
+- [API](#api)
+
+
+
+## Installation
+
+* 1.Run `npm i react-native-checkbox-component --save`
+* 2.`import CheckBox from 'react-native-checkbox-component'`    
+
+## Getting started  
+
+Add `react-native-checkbox-component` to your js file.   
+
+`import CheckBox from 'react-native-checkbox-component'`  
+
+Inside your component's render method, use CheckBox:   
+
+
+### Basic usage  
+
+```javascript
+<CheckBox
+    style={{flex: 1, padding: 10}}
+    onClick={()=>this.onClick(data)}
+    isChecked={data.checked}
+    leftText={leftText}
+/>;
+```
+
+### Custom CheckBox   
+
+```javascript
+renderCheckBox(data) {
+    var leftText = data.name;
+    return (
+    <CheckBox
+        style={{flex: 1, padding: 10}}
+        onClick={()=>this.onClick(data)}
+        isChecked={data.checked}
+        leftText={leftText}
+        checkedComponent={<Text>1</Text>}
+        unCheckedComponent={<Text>0</Text>}
+    />);
+}
+```
+
+## API
+
+
+Props              | Type     | Optional | Default     | Description
+----------------- | -------- | -------- | ----------- | -----------
+style  | ViewPropTypes.style  | true |   |   Custom style checkbox
+leftText | PropTypes.string |true |   | Custom left Text
+leftTextStyle  |  Text.propTypes.style | true |  | Custom left Text style
+rightText | PropTypes.string |true |   | Custom right Text
+rightTextView | PropTypes.element | true |   | Custom right TextView
+rightTextStyle  | Text.propTypes.style | true |  | Custom right Text style
+checkedComponent  |  PropTypes.element  | true  | Default Icon | Custom  checked Component
+unCheckedComponent  |  PropTypes.element  | true  |  Default Icon  | Custom  unchecked Component
+isChecked  |  PropTypes.bool |  false  |  false  | checkbox checked state
+onClick   |  PropTypes.func.isRequired |  false  |  | callback  function
+disabled  |  PropTypes.bool            | true  |  false | Disable the checkbox button
+checkBoxColor | PropTypes.string | true |   | Tint color of the checkbox image (this props is for both checked and unchecked state)
+checkedCheckBoxColor | PropTypes.string | true |   | Tint color of the checked state checkbox image (this prop will override value of `checkBoxColor` for checked state)
+uncheckedCheckBoxColor | PropTypes.string | true |   | Tint color of the unchecked state checkbox image (this prop will override value of `checkBoxColor` for unchecked state)
+
+
+
+
+**MIT Licensed**
---
+++ index.js
@@ -0,0 +1,142 @@
+/**
+ * react-native-custom-check-box
+ * Customizable Checkbox component for react native, it works on iOS and Android
+ * https://github.com/bgoyal2222/react-native-custom-check-box
+ * Email:bhaveshgoyal999@gmail.com
+ * Profile:https://www.linkedin.com/in/bhavesh-goyal/
+ */
+
+import React, { Component } from 'react';
+import {
+	StyleSheet,
+	View,
+	ViewPropTypes,
+	Image,
+	Text,
+	TouchableHighlight
+} from 'react-native';
+import PropTypes from 'prop-types';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default class CheckBox extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isChecked: this.props.isChecked,
+		}
+	}
+	static propTypes = {
+		...(ViewPropTypes || View.PropTypes),
+		leftText: PropTypes.string,
+		leftTextView: PropTypes.element,
+		rightText: PropTypes.string,
+		leftTextStyle: PropTypes.object,
+		rightTextView: PropTypes.element,
+		rightTextStyle: PropTypes.object,
+		checkedComponent: PropTypes.element,
+		unCheckedComponent: PropTypes.element,
+		onClick: PropTypes.func.isRequired,
+		isChecked: PropTypes.bool.isRequired,
+		checkBoxColor: PropTypes.string,
+		checkedCheckBoxColor: PropTypes.string,
+		uncheckedCheckBoxColor: PropTypes.string,
+		disabled: PropTypes.bool,
+	}
+	static defaultProps = {
+		isChecked: false,
+		leftTextStyle: {},
+		rightTextStyle: {}
+	}
+
+	static getDerivedStateFromProps(nextProps, prevState) {
+		if (prevState.isChecked !== nextProps.isChecked) {
+			return {
+				isChecked: nextProps.isChecked
+			};
+		}
+		return null;
+	}
+	onClick() {
+		const checkboxState = !this.state.isChecked;
+		this.setState({
+			isChecked: checkboxState
+		})
+		this.props.onClick(checkboxState);
+	}
+	_renderLeft() {
+		if (this.props.leftTextView) return this.props.leftTextView;
+		if (!this.props.leftText) return null;
+		return (
+			<Text style={[styles.leftText, this.props.leftTextStyle]}>{this.props.leftText}</Text>
+		);
+	}
+	_renderRight() {
+		if (this.props.rightTextView) return this.props.rightTextView;
+		if (!this.props.rightText) return null;
+		return (
+			<Text style={[styles.rightText, this.props.rightTextStyle]}>{this.props.rightText}</Text>
+		);
+	}
+
+	_renderImage() {
+		if (this.state.isChecked) {
+			return this.props.checkedComponent ? this.props.checkedComponent : this.genCheckedIcon();
+		} else {
+			return this.props.unCheckedComponent ? this.props.unCheckedComponent : this.genCheckedIcon();
+		}
+	}
+
+	_getCheckedCheckBoxColor() {
+		return this.props.checkedCheckBoxColor ? this.props.checkedCheckBoxColor : this.props.checkBoxColor
+	}
+
+	_getUncheckedCheckBoxColor() {
+		return this.props.uncheckedCheckBoxColor ? this.props.uncheckedCheckBoxColor : this.props.checkBoxColor
+	}
+
+	_getTintColor() {
+		return this.state.isChecked ? this._getCheckedCheckBoxColor() : this._getUncheckedCheckBoxColor()
+	}
+
+	genCheckedIcon() {
+		var source;
+		source = this.state.isChecked ? 'checkbox-marked' : 'checkbox-blank-outline';
+		return (
+			<MaterialCommunityIcons name={source} style={{}} size={20} color={this._getTintColor()} />
+		);
+	}
+	componentWillReceiveProps(nextProps) {
+		if (this.props.isChecked !== nextProps.isChecked) {
+			this.setState({ isChecked: nextProps.isChecked });
+		}
+	}
+	render() {
+		return (
+			<TouchableHighlight
+				style={this.props.style}
+				onPress={() => this.onClick()}
+				underlayColor='transparent'
+				disabled={this.props.disabled}
+			>
+				<View style={styles.container}>
+					{this._renderLeft()}
+					{this._renderImage()}
+					{this._renderRight()}
+				</View>
+			</TouchableHighlight>
+		);
+	}
+}
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	leftText: {
+		flex: 1,
+	},
+	rightText: {
+		flex: 1,
+		marginLeft: 10
+	}
+});
